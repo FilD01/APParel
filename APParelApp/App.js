a3540a8ac4df4602df7edbac61e789a495c40740
fildaems <fildaems@icloud.com>
2021-12-04 23:26:40 +0100

    big blue one

--- APParelApp/App.js
+++ APParelApp/App.js
@@ -44,28 +44,16 @@ function App() {
   return (
     <SafeAreaView style={backgroundStyle}>
         <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-            <Text>{data.nummer}</Text>
+            <Text style={styles.textStyle}>{data.nummer}</Text>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+    textStyle: {
+        color: 'blue',
+        fontSize: 60,
+    },
 });
 
 export default App;
